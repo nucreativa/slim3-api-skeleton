29761ff0fc3fa4cf3adce75510f533c6be64ed22
Ary Wibowo <nucreativa@gmail.com>
2015-11-11 06:36:00 +0700

    Fix Log mispath and update version to 0.0.2

--- app/middlewares.php
+++ app/middlewares.php
@@ -10,7 +10,7 @@ $checkProxyHeaders = true; // Note: Never trust the IP address for security proc
 $trustedProxies = ['10.0.0.1', '10.0.0.2']; // Note: Never trust the IP address for security processes!
 $app->add(new \RKA\Middleware\IpAddress($checkProxyHeaders, $trustedProxies));
 
-$stream = new \Monolog\Handler\StreamHandler(__DIR__ . '/../../log/app.log', \Monolog\Logger::DEBUG);
+$stream = new \Monolog\Handler\StreamHandler(__DIR__ . '/../log/app.log', \Monolog\Logger::DEBUG);
 $logger = new \Monolog\Logger('app');
 $logger->pushHandler($stream);
 $app->add(function ($request, $response, $next) use ($logger) {
